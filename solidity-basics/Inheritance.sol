d456c56bde25adec25c20df6765149b5d88e1688
parash.a <parash.a@namespace.jp>
2021-03-10 14:05:22 +0545

    Inheritance in solidity with factory

---
+++ solidity-basics/Inheritance.sol
@@ -0,0 +1,40 @@
+pragma solidity ^0.7.0;
+
+contract Ownable {
+    address owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner {
+        require( msg.sender == owner,'Must be Owner.'); _;
+    }
+}
+
+contract SecretVault {
+    string secret;
+    
+    constructor(string memory _secret) {
+        secret = _secret;
+    }
+
+    function getSecret() public view returns(string memory) {
+        return secret;
+    }
+}
+
+contract Inheritance is Ownable {
+    address secretVault;
+    
+    constructor(string memory _secret) {
+        SecretVault _secretVault = new SecretVault(_secret);
+        secretVault = address(_secretVault);
+        super;
+    }
+
+    function getSecret() public view onlyOwner returns(string memory) {
+        SecretVault _secretVault = SecretVault(secretVault);
+        return _secretVault.getSecret();
+    }
+}
